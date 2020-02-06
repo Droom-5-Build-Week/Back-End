1ef578dfc206cc0192ee5f415b51e9719dd81ea8
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-06 13:47:06 -0800

    companies and users tests

--- companies/companies-router.spec.js
+++ companies/companies-router.spec.js
@@ -4,17 +4,7 @@ const db = require('../database/db-config.js');
 
 describe('company-router', function () {
 
-    beforeEach(async function () {
-        await db('companies').truncate();
-    })
-
-    describe('test environment', function () {
-        it('should be using test env', function () {
-            expect(process.env.DB_ENV).toBe('testing');
-        })
-    })
-
-    const newCompany = {
+        const newCompany = {
         email: "bill@microsoft.com",
         password: "bill",
         name: "Microsoft",
@@ -28,50 +18,60 @@ describe('company-router', function () {
         location: "USA"
     }
 
-    describe('get all companies works', function () {
+    beforeEach(async function () {
+        await db('companies').truncate();
+    })
 
-        it('should return a 200', async function () {
-            await request(server).post('/api/auth/register').send(newCompany)
-            await request(server).post('/api/auth/login').send(newCompany)
+    describe('test environment', function () {
+        it('should be using test env', function () {
+            expect(process.env.DB_ENV).toBe('testing');
+        })
+    })
+
+    describe('/api/companies', function () {
+
+        it('GET all companies and return a 200', async function () {
+            await request(server).post('/api/auth/company/register').send(newCompany)
+            await request(server).post('/api/auth/company/login').send(newCompany)
                 .then(async company => {
-                    const res = await request(server).get('/api/companies').set('authorization', company.body.token)
+                    const res = await request(server).get('/api/companies')
+                        .set('authorization', company.body.token)
                     expect(res.status).toBe(200);
                 })
         })
     })
 
-    describe('get company by id works', function () {
+    describe('/api/companies/:id', function () {
 
-        it('should return a 200', async function () {
-            await request(server).post('/api/auth/register').send(newCompany)
-            await request(server).post('/api/auth/login').send(newCompany)
+        it('Get company by id status 200', async function () {
+            await request(server).post('/api/auth/company/register').send(newCompany)
+            await request(server).post('/api/auth/company/login').send(newCompany)
                 .then(async company => {
                     const res = await request(server).get(`api/companies/1`).set('authorization', company.body.token)
                     expect(res.status).toBe(200);
                 })
         })
-    })
 
-    //something
-
-    describe('edit company by id', function () {
-
-        it('should return a 200', async function () {
-            await request(server).post('/api/auth/register').send(newCompany)
-            await request(server).post('/api/auth/login').send(newCompany)
+        it('Get company by id', async function () {
+            await request(server).post('/api/auth/company/register').send(newCompany)
+            await request(server).post('/api/auth/company/login').send(newCompany)
                 .then(async company => {
-                    const id = company.body.id;
-                    const res = await request(server).put(`api/companies/:id`, newerCompany).set('authorization', company.body.token)
-                    expect(res.body.name).toBe("NotMicrosoft");
+                    const res = await request(server).get(`api/companies/1`).set('authorization', company.body.token)
+                    expect(res.body.id).toBe(1);
                 })
         })
+
+        // it('Get company by id', async function () {
+        //     await request(server).post('/api/auth/company/register').send(newCompany)
+        //     await request(server).post('/api/auth/company/login').send(newCompany)
+        //         .then(async company => {
+        //             const id = company.body.id;
+        //             const res = await request(server)
+        //                 .put(`api/companies/1`, newerCompany)
+        //                 .set('authorization', company.body.token)
+        //             expect(res.body.name).toBe("Melinda Gates Foundation");
+        //         })
+        // })
     })
+
 })
-// it('GET all users', async function () {
-//     await request(server).post('/api/auth/register').send(user)
-//     await request(server).post('/api/auth/login').send(user)
-//         .then(async user => {
-//             const res = await request(server).get('/api/users').set('authorization', user.body.token)
-//             expect(res.status).toBe(200);
-//         })
-// })
