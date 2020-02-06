4191a0a4257e97966d241354e43888638a8f7540
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-02-06 08:38:52 -0800

    testing environment ready for testing

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
       directory: './database/migrations',
     },
     seeds: {
-      directory: '.database/seeds',
+      directory: './database/seeds',
     },
   },
   production: {
